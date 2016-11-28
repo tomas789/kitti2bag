fcf4e3f21db0bc34a60b40efc1b3c66e598278e0
Tom <tom@salesito.com>
2016-11-28 13:08:46 +0100

    prepare for pypi

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(name='kitti2bag',
       author='Tomas Krejci',
       author_email='tomas789@gmail.com',
       url='https://github.com/tomas789/kitti2bag/',
+      download_url = 'https://github.com/tomas789/kitti2bag/archive/1.0',
+      keywords = ['dataset', 'ros', 'rosbag', 'kitti'],
       scripts=['bin/kitti2bag']
      )
